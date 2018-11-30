5daf5b32c49607dd7df8fb0ab98c13854305e98a
Bolum Victor <vbolum@epayplusng.com>
2018-11-30 17:34:12 +0100

    add unit test

---
+++ CompaniesFleet.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CompaniesFleet.Controllers;
+
+namespace CompaniesFleet.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            CompanyCarController companyCar = new CompanyCarController();
+
+            var result = companyCar.Get();
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
